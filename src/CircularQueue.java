2b93067201a9e1bec676028e1e46f3d4fe530037
xichen816 <128641225+xichen816@users.noreply.github.com>
2024-02-08 16:09:47 -0500

    méthodes

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -1,2 +1,14 @@
 public class CircularQueue {
+    public void reverse(){
+
+    }
+
+    public boolean checkInQueue(int value){
+
+        return false;
+    }
+
+    public void remove(int value){
+
+    }
 }
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,28 @@
 public class LinkedList {
+    public void removeValue(int value){
 
+    }
+
+    public int returnNLast(int nLast){
+
+        return nLast;
+    }
+
+    public boolean checkInList(int value){
+
+        return false;
+    }
+
+    public int maxValue(){
+
+        return 0;
+    }
+
+    public void insertionSort(){
+
+    }
+
+    public void addInOrder(int value){
+
+    }
 }
