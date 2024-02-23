a006cbd8d7fb18d1c2c5d71cbadf7245c90ac775
xichen816 <128641225+xichen816@users.noreply.github.com>
2024-02-23 16:18:22 -0500

    Update

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -2,13 +2,31 @@
  * Classe représentant une file d’attente circulaire
  */
 public class CircularQueue {
+    private int size;
+    private int front, rear;
 
     /**
      * Inverser l’ordre de la file d’attente circulaire
      */
+
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
     public void reverse(){
 
     }
+    public void enqueue(int value){
+
+    }
+    public void dequeue(){
+
+    }
+
 
     /**
      * Vérifie si l'élément est dans la file d'attente
